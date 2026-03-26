f13d0458dead458b0bbe58fe547707c9ac9356d8
Sanyi93 <sandor.tomas93@gmail.com>
2026-03-26 22:22:24 +0100

    authSetupFile basics

---
+++ tests/auth.setup.ts
@@ -0,0 +1,33 @@
+import path from 'path';
+import fs from 'fs';
+import test, { expect } from '@playwright/test'
+
+//TODO: rewrite imports after fixture file completion
+
+
+const authFileNormalUser = path.join('.auth', './normal_user.json');
+const authFileAdminUser = path.join('.auth', './admin_user.json');
+
+test('Authenticate normal user', async ({ page }) => {
+    await page.goto('/');
+    
+    //TODO loginProcess after the LoginPage POM & Fixture Completion
+
+    if(!fs.existsSync('.auth')){
+        fs.mkdirSync('auth');
+    }
+
+    await page.context().storageState({ path: authFileNormalUser});
+});
+
+test('Authenticate admin user', async({ page }) => {
+    await page.goto('/');
+
+    //TODO LoginProcess after the LoginPage POM & Fixture Completion
+
+        if(!fs.existsSync('.auth')){
+        fs.mkdirSync('auth');
+    }
+
+    await page.context().storageState({ path: authFileAdminUser});
+})
