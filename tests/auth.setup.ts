4579142107bcff54940d401899fcefef3c0f6612
Sanyi93 <sandor.tomas93@gmail.com>
2026-03-27 17:54:00 +0100

    authSetup imports adjusted

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,9 +1,6 @@
 import path from 'path';
 import fs from 'fs';
-import test, { expect } from '@playwright/test'
-
-//TODO: rewrite imports after fixture file completion
-
+import test, { expect } from '../myFixture';
 
 const authFileNormalUser = path.join('.auth', './normal_user.json');
 const authFileAdminUser = path.join('.auth', './admin_user.json');
