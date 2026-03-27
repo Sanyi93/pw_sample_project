f142b6638081f35637949001ecb37bb98b54a1fe
Sanyi93 <sandor.tomas93@gmail.com>
2026-03-28 00:57:52 +0100

    API test notes & files

---
+++ controllers/BookingController.ts
@@ -0,0 +1,8 @@
+import { APIRequestContext } from "@playwright/test";
+
+export class BookingController{
+    constructor ( private request : APIRequestContext){}
+
+    //TODOD: Basic API methods -> create a booking - check if created - update(+auth) - check - delete
+    //
+}
---
+++ tests/api.spec.ts
@@ -0,0 +1,1 @@
+//TODO: API Tests via controller
