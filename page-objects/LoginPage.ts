6747630f68f4632faeffb806fe9a4bacafde1d14
Sanyi93 <sandor.tomas93@gmail.com>
2026-03-28 00:15:28 +0100

    config typo & LoginPage adjusted

--- page-objects/LoginPage.ts
+++ page-objects/LoginPage.ts
@@ -5,11 +5,15 @@ export class LoginPage{
     usernamefield: Locator;
     userpasswordfield: Locator;
     loginButton: Locator;
+    errorButton: Locator;
+    passwordValidationElement: Locator;
 
     constructor(page: Page){
         this.page = page;
         this.usernamefield = page.getByPlaceholder('Username');
         this.userpasswordfield = page.getByPlaceholder('Password');
         this.loginButton = page.getByRole('button', { name: 'Login'});
+        this.errorButton = page.locator('//button[@class = "error-button"]');
+        this.passwordValidationElement = page.locator('//h3[@data-test = "error"]');
     }
 }
