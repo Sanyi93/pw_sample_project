e21ecb793937a101b56f132e4c56e4effed582ac
Sanyi93 <sandor.tomas93@gmail.com>
2026-03-29 16:24:35 +0200

    test step annotations added

--- tests/basic-testcases.spec.ts
+++ tests/basic-testcases.spec.ts
@@ -27,32 +27,40 @@ test('Incorrect login test', async({ checkVisually, loginPage }) => {
 });
 
 test('Basic Purchase Workflow', async ({ checkVisually, inventoryPage, page}) => {  
-  await page.goto('/inventory.html');
-  await page.waitForLoadState('domcontentloaded');
-  await checkVisually('Inventory-Page-After-Login', { scrollableContent: page.locator('//div[@id="page_wrapper"]')});
+  
+  await test.step('Landing on the page', async() => {
+    await page.goto('/inventory.html');
+    await page.waitForLoadState('domcontentloaded');
+    await checkVisually('Inventory-Page-After-Login', { scrollableContent: page.locator('//div[@id="page_wrapper"]')});
+  })
 
-  await inventoryPage.backpackAddToCartButton.click();
-  expect(inventoryPage.backpackRemoveFromCartButton).toBeVisible();
-  await checkVisually('Cart-Icon-After-Item-Added', { elementContent: inventoryPage.shoppingCartIcon});
-  expect(inventoryPage.shoppingCartBadge).toBeVisible();
-  expect(inventoryPage.shoppingCartBadge).toHaveText('1');
-  await inventoryPage.shoppingCartIcon.click();
-  await checkVisually('Item-In-Shopping-Cart');
-  await inventoryPage.checkoutButton.click();
+  await test.step('Adding item to the cart and proceesing to checkout', async() => {
+    await inventoryPage.backpackAddToCartButton.click();
+    expect(inventoryPage.backpackRemoveFromCartButton).toBeVisible();
+    await checkVisually('Cart-Icon-After-Item-Added', { elementContent: inventoryPage.shoppingCartIcon});
+    expect(inventoryPage.shoppingCartBadge).toBeVisible();
+    expect(inventoryPage.shoppingCartBadge).toHaveText('1');
+    await inventoryPage.shoppingCartIcon.click();
+    await checkVisually('Item-In-Shopping-Cart');
+    await inventoryPage.checkoutButton.click();
+  })
 
-  await inventoryPage.firstNameField.click();
-  await inventoryPage.firstNameField.fill("Jake");
-  await inventoryPage.lastNameField.click();
-  await inventoryPage.lastNameField.fill('Hudson');
-  await inventoryPage.postalCodeField.click();
-  await inventoryPage.postalCodeField.fill('1120');
-  await checkVisually('Checkout-Information-Filled');
-  await inventoryPage.continueButton.click();
-  await checkVisually('Checkout-Overview');
-  await inventoryPage.finishButton.click();
-  await checkVisually('Checkout-Completed');
+  await test.step('Filling out the checkout form', async() => {
+    await inventoryPage.firstNameField.click();
+    await inventoryPage.firstNameField.fill("Jake");
+    await inventoryPage.lastNameField.click();
+    await inventoryPage.lastNameField.fill('Hudson');
+    await inventoryPage.postalCodeField.click();
+    await inventoryPage.postalCodeField.fill('1120');
+    await checkVisually('Checkout-Information-Filled');
+    await inventoryPage.continueButton.click();
+    await checkVisually('Checkout-Overview');
+    await inventoryPage.finishButton.click();
+    await checkVisually('Checkout-Completed');
+  })
 
-  await inventoryPage.backHomeButton.click();
-  await checkVisually('Inventory-Home-Page-After-Successfull-Checkout');
-
+  await test.step('Proceeding to the home page', async() => {
+    await inventoryPage.backHomeButton.click();
+    await checkVisually('Inventory-Home-Page-After-Successfull-Checkout');
+  })
 })
