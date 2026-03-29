fcc932f59ad0f8ecd15b35fc81b157d243b0727d
Sanyi93 <sandor.tomas93@gmail.com>
2026-03-29 14:50:19 +0200

    inventoryPage elements added

--- page-objects/InventoryPage.ts
+++ page-objects/InventoryPage.ts
@@ -2,10 +2,30 @@ import { Locator, Page } from '@playwright/test';
 
 export class InventoryPage{
     page: Page;
+    backpackAddToCartButton: Locator;
+    backpackRemoveFromCartButton: Locator;
+    shoppingCartIcon: Locator;
+    checkoutButton: Locator;
+    firstNameField: Locator;
+    lastNameField: Locator;
+    postalCodeField: Locator;
+    continueButton: Locator;
+    finishButton: Locator;
+    backHomeButton: Locator;
+    
+
 
     constructor(page: Page){
         this.page = page;
-        
-
+        this.backpackAddToCartButton = page.locator('[data-test="add-to-cart-sauce-labs-backpack"]');
+        this.backpackRemoveFromCartButton = page.locator('[data-test="remove-sauce-labs-backpack"]');
+        this.shoppingCartIcon = page.locator('[data-test="shopping-cart-link"]');
+        this.firstNameField = page.getByPlaceholder('First Name');
+        this.lastNameField = page.getByPlaceholder('Last Name');
+        this.postalCodeField = page.getByPlaceholder('Zip/Postal Code');
+        this.checkoutButton = page.getByRole('button', { name: 'Checkout'});
+        this.continueButton = page.getByRole('button', { name: 'Continue'});
+        this.finishButton = page.getByRole('button', { name: 'Finish'});
+        this.backHomeButton = page.getByRole('button', { name: 'Back Home'});
     }
 }
