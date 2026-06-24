0c8dee9e1752c9a71922a94531528f558efcfd21
Sanyi93 <sandor.tomas93@gmail.com>
2026-06-24 22:22:41 +0200

    setup fuer CI & local konfiguriert

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -5,7 +5,17 @@ import test, { expect } from '../myFixture';
 const authFileNormalUser = path.resolve('.auth', './normal_user.json');
 const authFileAdminUser = path.resolve('.auth', './admin_user.json');
 
+const USERNAME_N = process.env.USERNAME_N;
+const PWD_N = process.env.PWD_N;
+
+const USERNAME_A = process.env.USERNAME_A;
+const PWD_A = process.env.PWD_A;
+
+
 test('Authenticate normal user', async ({ loginPage, page }) => {
+    if(!USERNAME_N || !PWD_N){
+        throw new Error('Missing credential variables for normal user - configure local variables or CI github secrets')
+    }
     await page.goto('/');
     await loginPage.usernamefield.click();
     await loginPage.usernamefield.fill(process.env.USERNAME_N!);
@@ -22,6 +32,9 @@ test('Authenticate normal user', async ({ loginPage, page }) => {
 });
 
 test('Authenticate admin user', async({ loginPage, page }) => {
+    if(!USERNAME_A || !PWD_A){
+        throw new Error('Missing credential variables for admin - configure local variables or CI github secrets')
+    }
     await page.goto('/');
     await loginPage.usernamefield.click();
     await loginPage.usernamefield.fill(process.env.USERNAME_A!);
